743b1e4d0151e67cbb02ff7dd9dca0b28a7e9351
DiegoForgan <dmforgan89@gmail.com>
2017-11-05 22:59:30 -0300

    arreglo de la app principal

--- src/main/java/ar/uba/fi/tdd/rulogic/App.java
+++ src/main/java/ar/uba/fi/tdd/rulogic/App.java
@@ -6,13 +6,15 @@ import ar.uba.fi.tdd.rulogic.model.KnowledgeBase;
 import java.util.Scanner;
 
 
+
+
 /**
  * Console application.
  *
  */
 public class App
 {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 
 		System.out.println("I shall answer all your questions!");
 		String query;
@@ -21,11 +23,11 @@ public class App
 		Scanner in = new Scanner(System.in);
 		while (keepAsking){
 			System.out.println("Enter your query:");
-			query = in.next();
+			query = in.nextLine();
 			System.out.println(baseDeDatos.answer(query));
 			System.out.println("Do you wish to keep asking? (s/n)");
-			if (in.next().equals("s")) keepAsking = true;
-			else keepAsking = false;
+			if (in.nextLine().equals("s")) keepAsking= true;
+			else {keepAsking = false;}
 		}
 	}
 }
